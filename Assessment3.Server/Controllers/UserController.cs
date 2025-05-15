8cc07e5f38132bac8d19e08a5fd5cf756f00dc47
mataquet <margot.taquet@etu.uca.fr>
2025-05-15 20:13:30 +0200

    create JWTService

---
+++ Assessment3.Server/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Assessment3.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assessment3.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
---
+++ Assessment3.Server/Service/JWTService.cs
@@ -0,0 +1,39 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Assessment3.Server.Models;
+
+namespace Assessment3.Server.Service;
+
+public class JWTService
+{
+    private readonly IConfiguration _configuration;
+    public JWTService(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_configuration["JwtSettings:SecretKey"]);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Login),
+            }),
+            Expires = DateTime.UtcNow.AddDays(1),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+    
+
+}
